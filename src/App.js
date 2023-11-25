483cd0c3fceef42d37f44fdc6785f25f881a4cf7
karthik <karthi.m34999@gmail.com>
2023-11-25 10:40:46 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <h1> welcome to aws amplify</h1>
+        <p>login is successful</p>
+      </head>
+    </html>
+  );
+}
+
+export default App;
